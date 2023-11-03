4361252175177c8fa61d5be687b29b0c8b098e50
Stepan Boychuk <demonomiconnnn@gmail.com>
2023-11-03 13:24:25 +0200

    Using fs.readFileSync

--- process-big-file.js
+++ process-big-file.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
-const m = new Map()
 
-const stream = fs.createReadStream('free_company_dataset.csv');
-
-stream.on('error', error => console.error(error));
-stream.on('data', chunk => m.set(chunk));
-stream.on('end', () => console.log('End'));
+try {
+    const fileContent = fs.readFileSync('free_company_dataset.csv');
+}catch(error) {
+    console.error(error);
+}
